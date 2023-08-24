faf3c095729c999ed56d8ee720f2fe76c9c184b3
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-08-24 21:04:30 +0700

    first commit

--- src/pages/detailPage.jsx
+++ src/pages/detailPage.jsx
@@ -1,18 +1,41 @@
 import Axios from "axios";
 import { Navbar } from "../components/navbar";
 import { Footer } from "../components/homePage/footer";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { DeleteButton } from "../components/deleteItem";
-import { CheckCircleIcon } from "@chakra-ui/icons";
+import { ArrowBackIcon, ArrowForwardIcon, CheckCircleIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from "react";
-import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
 export const DetailPage = () => {
     const params = useParams();
-    const username = useSelector((state) => state.user.value.username)
+    const username = useSelector((state) => state.user.value.username);
+    const [user, setUser] = useState();
     const [data, setData] = useState();
-    const [user, setUser] = useState()
+    const [data2, setData2] = useState();
+    const [page, setPage] = useState(1);
+    const [totalPage, setTotalPage] = useState(1);
+    const navigate = useNavigate();
+    const getPagination = async (pageNum) => {
+        try {
+            const response = await Axios.get(`https://minpro-blog.purwadhikabootcamp.com/api/blog?page=${pageNum}&size=8`, data);
+            setData2(response.data.result);
+            setPage(response.data.blogPage);
+            setTotalPage(response.data.page);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const goToPrevPage = () => {
+        if (page > 1) getPagination(page - 1);
+    }
+    const goToNextPage = () => {
+        if (page < totalPage) getPagination(page + 1);
+    }
+    const handleClick = (id) => {
+        navigate(`detailPage/${id}`);
+    }
     const getarticel = async (data) => {
         try {
             const response = await Axios.get(`https://minpro-blog.purwadhikabootcamp.com/api/blog/${params.id}`, data);
@@ -23,6 +46,7 @@ export const DetailPage = () => {
         }
     }
     useEffect(() => {
+        getPagination()
         getarticel();
     }, []);
     return (
@@ -79,7 +103,74 @@ export const DetailPage = () => {
                     <CheckCircleIcon /> ‎ Like this article
                 </Button>
             </Flex>
-            <Flex mb={"0px"} mt={"10px"}>
+            <Flex>
+                <Box
+                    border={"1px solid gray"}
+                    w={["full", "full"]} bg={"gray.200"}
+                    pt={"10px"} mt={"22px"}>
+                    <Flex justifyContent={"center"}>
+                        <Heading fontSize={["20px", "30px"]} fontFamily={"mono"} color={"#71B280"}>Suggestion For You</Heading>
+                    </Flex>
+                    <Flex flexWrap={"wrap"} pb={"10px"}
+                        justifyContent={"center"}>
+                        {data2?.map((item, index) => {
+                            return (
+                                <>
+                                    <Box key={index} onClick={() => handleClick(item.id)}
+                                        _hover={{ transform: "scale(1.1)" }}
+                                        bg={"white"}
+                                        w={["150px", "170px", "250px"]} h={["200px", "220px"]}
+                                        my={"10px"} mx={"10px"}
+                                        border={"3px solid #408E91"}
+                                        borderRadius={"5px"}
+                                        boxShadow='0px 0px1 px'>
+                                        <Flex justifyContent={"center"}>
+                                            <Flex justifyContent={"center"} bg={"white"}
+                                                border={"3px solid #408E91"}
+                                                borderRadius={"10px"} mt={"16px"} mx={["auto"]}
+                                                w={["120px", "150px", "200px"]} h={["80px", "100px"]} >
+                                                <Image src={`https://minpro-blog.purwadhikabootcamp.com/${item.imageURL}`} ></Image>
+                                            </Flex>
+                                        </Flex>
+                                        <Text mt={"3px"}
+                                            overflow="hidden"
+                                            whiteSpace="nowrap"
+                                            textOverflow="ellipsis"
+                                            maxWidth={["130px", "195px"]}
+                                            ml={["12px", "15px", "28px"]} fontSize={"20px"} fontFamily={"monospace"} color={"black"}>
+                                            {item.title}
+                                        </Text>
+                                        <Flex mt={["12px", "6px"]} ml={["10px", "10px", "20px"]} >
+                                            <Avatar src={`https://minpro-blog.purwadhikabootcamp.com/${item.User.imgProfile}`} size={["sm", "md"]} mt={["8px", '0px']} />
+                                            <Box mt={"3px"}>
+                                                <Text
+                                                    overflow="hidden"
+                                                    whiteSpace="nowrap"
+                                                    textOverflow="ellipsis"
+                                                    maxWidth={["80px", "100px", "165px"]}
+                                                    ml={"10px"} fontSize={"14px"} fontFamily={"monospace"} color={"black"}
+                                                >
+                                                    by {item.User.username}
+                                                </Text>
+                                                <Text ml={"10px"} fontSize={"12px"} fontFamily={"monospace"} color={"black"}>
+                                                    {item.Category.name}
+                                                </Text>
+                                            </Box>
+
+                                        </Flex>
+                                    </Box>
+                                </>
+                            );
+                        })}
+                    </Flex>
+                    <Flex justifyContent={"center"} my="10px">
+                        <Button bgColor="#408E91" color="white" onClick={goToPrevPage} disabled={page === 1}><ArrowBackIcon /></Button>
+                        <Button mx="5px" disabled bgColor="#408E91" color="white">{page}</Button>
+                        <Button onClick={goToNextPage} disabled={page === totalPage} bgColor="#408E91" color="white"><ArrowForwardIcon /></Button>
+                    </Flex>
+                </Box>
+            </Flex>
+            <Flex mb={"0px"} mt={"0px"}>
                 <Footer />
             </Flex>
         </>
